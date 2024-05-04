8048a1ac8c4352d44e778452acb943883a00ce76
zhonghao <124886877@qq.com>
2024-05-04 21:31:37 +0800

    update robot

--- util/robot_util.py
+++ util/robot_util.py
@@ -4,7 +4,7 @@ from util.request_util import HttpRequester
 
 
 def send_feishu_msg(msg):
-    robot_url = "https://www.feishu.cn/flow/api/trigger-webhook/cfa1ad0fe13ae045a41528c20fd3c667"
+    robot_url = "https://www.feishu.cn/flow/api/trigger-webhook/ccfa1ad0fe13ae045a41528c20fd3c667111"
     requester = HttpRequester()
     post_response = requester.post(robot_url, json={"user": msg},
                                    headers={"Content-Type": "application/json"})
